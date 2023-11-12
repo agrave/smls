07001092ed17dda76ab68bbf7299bfbf944c7103
Artem Mohylnykov <artem.mohylnykov@tui.be>
2023-11-12 22:40:53 +0200

    use IIFE call

--- index.ts
+++ index.ts
@@ -3,14 +3,12 @@ import { getMarks } from './src/marks'
 import { calcBonus } from './src/bonus'
 import { printTable } from 'console-table-printer'
 
-async function main(): Promise<void> {
+void (async function (): Promise<void> {
   const students = JSON.parse(process.env.STUDENTS as string)
 
   for await (const student of students) {
     const marks = calcBonus(await getMarks(student.id))
-    console.log(student.name)
+    console.log('\n', student.name)
     printTable(marks)
   }
-}
-
-void main()
+})()
